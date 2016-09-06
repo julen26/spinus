1e1aa4b1025d91d06167505ea057c56d834c7c47
Julen Salgado <julensalgado@gmail.com>
2016-09-06 12:16:44 +0200

    Close #21, refs #20 Improve performance with automatic mipmaping management.
    - Generate mipmaps only for power-of-two textures.
    - Allow to force mipmap generation, resizing texture to POT.
    - Tiling only will be available for POT textures.

--- src/graphics/Texture.js
+++ src/graphics/Texture.js
@@ -5,11 +5,11 @@ goog.require('sp.Vector2');
 * Constructs Texture objects
 * @class Represents a Texture object
 */
-sp.Texture = function(context, sourcePath, callback, smooth, repeat, useMipmap) {
+sp.Texture = function(context, sourcePath, callback, smooth, repeat, forcePOT) {
 	this.m_context = context;
     this.m_smooth = smooth || false;
     this.m_repeat = repeat || false;
-    this.m_useMipmap = useMipmap || false;
+    this.m_forcePOT = forcePOT || false;
     this.m_image = new Image();
     if (sourcePath) {
         this.loadFromFile(sourcePath, callback || null);
@@ -42,30 +42,31 @@ sp.Texture.prototype.handleLoadedTexture = function () {
     	this.m_textureId = gl.createTexture();
     	gl.bindTexture(gl.TEXTURE_2D, this.m_textureId);
 
-        var tmpImage = this.m_image;
-
-        if ((this.m_repeat || this.m_useMipmap) && !this.isPowerOfTwo()) {
+        if (this.forcePOT && !this.isPowerOfTwo()) {
             var ctx = document.createElement("canvas").getContext("2d");
             ctx.canvas.width = this.nextHighestPowerOfTwo(this.m_size.x);
             ctx.canvas.height = this.nextHighestPowerOfTwo(this.m_size.y);
-            ctx.drawImage(tmpImage, 0, 0, tmpImage.width, tmpImage.height);
-            tmpImage = ctx.canvas;
+            ctx.drawImage(this.m_image, 0, 0, this.m_image.width, this.m_image.height);
             this.m_size.x = ctx.canvas.width;
             this.m_size.y = ctx.canvas.height;
+            this.m_image = ctx.canvas;
         }
 
-    	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, tmpImage);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, (this.m_repeat) ? gl.REPEAT : gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, (this.m_repeat) ? gl.REPEAT : gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, (this.m_smooth) ? gl.LINEAR : gl.NEAREST);
-        //TODO: Implement filter types (bilinear, trilinear)
-        if (this.m_useMipmap) {
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, (this.m_smooth) ? gl.LINEAR_MIPMAP_NEAREST : gl.NEAREST_MIPMAP_NEAREST);
+    	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.m_image);
+        if (this.isPowerOfTwo()) {
             gl.generateMipmap(gl.TEXTURE_2D);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, (this.m_smooth) ? gl.LINEAR_MIPMAP_NEAREST : gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, (this.m_repeat) ? gl.REPEAT : gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, (this.m_repeat) ? gl.REPEAT : gl.CLAMP_TO_EDGE);
         }
         else {
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, (this.m_smooth) ? gl.LINEAR : gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            this.m_repeat = false;
         }
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, (this.m_smooth) ? gl.LINEAR : gl.NEAREST);
+
     	gl.bindTexture(gl.TEXTURE_2D, null);
 
         if (this.m_callback) {
@@ -79,6 +80,32 @@ sp.Texture.prototype.getTextureId = function () {
 
 sp.Texture.prototype.getSize = function () {
     return this.m_size;
+};
+
+sp.Texture.prototype.setSmooth = function (smooth) {
+    gl.bindTexture(gl.TEXTURE_2D, this.m_textureId);
+    if (this.isPowerOfTwo()) {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, (this.m_smooth) ? gl.LINEAR_MIPMAP_NEAREST : gl.NEAREST);
+    }
+    else {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, (this.m_smooth) ? gl.LINEAR : gl.NEAREST);
+    }
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, (this.m_smooth) ? gl.LINEAR : gl.NEAREST);
+    this.m_smooth = smooth;
+};
+
+sp.Texture.prototype.setRepeat = function (repeat) {
+    gl.bindTexture(gl.TEXTURE_2D, this.m_textureId);
+    if (this.isPowerOfTwo()) {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, (repeat) ? gl.REPEAT : gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, (repeat) ? gl.REPEAT : gl.CLAMP_TO_EDGE);
+        this.m_repeat = repeat;
+    }
+    else {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        this.m_repeat = false;
+    }
 };
 
 sp.Texture.prototype.isPowerOfTwo = function () {
