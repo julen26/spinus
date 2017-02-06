18cbecdbdb6e7c05db1cbdb8ab0adb91dbd1b580
Julen Salgado <julensalgado@gmail.com>
2017-02-06 10:53:01 +0100

    Add bluelabel-jsdoc template for documentation pages.

--- builder/build-doc.bat
+++ builder/build-doc.bat
@@ -1,3 +1,3 @@
 rmdir /s /q ..\doc
-java -jar ..\tools\jsdoc-toolkit\jsrun.jar ..\tools\jsdoc-toolkit\app\run.js ..\src\* -t=..\tools\jsdoc-toolkit\templates\jsdoc -d=..\doc
+java -jar ..\tools\jsdoc-toolkit\jsrun.jar ..\tools\jsdoc-toolkit\app\run.js ..\src\* -t=..\tools\jsdoc-toolkit\templates\bluelabel-jsdoc -d=..\doc
 pause
