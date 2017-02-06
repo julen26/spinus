24cdb02cb833caee8f5c6d17f2db3ca55ab59ddd
Julen Salgado <julensalgado@gmail.com>
2017-02-06 15:59:27 +0100

    Add global namespace definition file.

---
+++ src/core/Global.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 Julen Salgado. All rights reserved.
+
+/**
+ * @fileoverview Global declarations
+ * @author Julen Salgado (julensalgado@gmail.com)
+ */
+
+ goog.provide('sp');
+
+ /**
+ * @namespace Main Spinus 2D framework namespace
+ */
+ var sp = sp || {};
