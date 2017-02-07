bb4ecd5f9f2925fc31822313babff9bb0e4222e7
Julen Salgado <julensalgado@gmail.com>
2017-02-07 13:34:01 +0100

    Change directory structure for documentation.

--- builder/build-doc.sh
+++ builder/build-doc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -rf ../doc
-java -jar ../tools/jsdoc-toolkit/jsrun.jar ../tools/jsdoc-toolkit/app/run.js ../src/* -t=../tools/jsdoc-toolkit/templates/bluelabel-jsdoc -d=../doc
+rm -rf ../doc/api
+java -jar ../tools/jsdoc-toolkit/jsrun.jar ../tools/jsdoc-toolkit/app/run.js ../src/* -t=../tools/jsdoc-toolkit/templates/bluelabel-jsdoc -d=../doc/api
