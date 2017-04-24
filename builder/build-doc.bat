1080b794190a36ba9a3d9cbbff58f19731ff59bb
Julen Salgado <j.salgado@ibermatica.com>
2017-04-24 10:51:02 +0200

    Fix tryit visualization and doc builder.

--- builder/build-doc.bat
+++ builder/build-doc.bat
@@ -1,3 +1,4 @@
 rmdir /s /q ..\docs\api
 java -jar ..\tools\jsdoc-toolkit\jsrun.jar ..\tools\jsdoc-toolkit\app\run.js ..\src\* -t=..\tools\jsdoc-toolkit\templates\bluelabel-jsdoc -d=..\docs\api
+copy /y ..\release\spinus.js ..\docs\tryit\spinus\
 pause
--- builder/build-doc.sh
+++ builder/build-doc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rm -rf ../docs/api
 java -jar ../tools/jsdoc-toolkit/jsrun.jar ../tools/jsdoc-toolkit/app/run.js ../src/* -t=../tools/jsdoc-toolkit/templates/bluelabel-jsdoc -d=../docs/api
+cp ../release/spinus.js ../docs/tryit/spinus/
